fe1b460094dbb54a94e13289d27b629d8febf075
Michal1898 <michal.volf@gmail.com>
2021-02-05 14:25:20 +0100

    přidána lru cache.

--- fibo_func.py
+++ fibo_func.py
@@ -1,40 +1,51 @@
 import timeit
+from functools import lru_cache
 
 class CustomException(Exception):
     def __init__(self,a=""):
         message = a
         super().__init__(message)
 
+@lru_cache()
+def fib_rec(n: int) -> int:
+    """Fibonacci - Recursive."""
+    if n < 1:
+        return 0
+    if n < 2:
+        return 1
+    return fib_rec(n - 1) + fib_rec(n - 2)
+@lru_cache()
+def fib_iter(n: int) -> int:
+    """Fibonacci - Iterative."""
+    first_number, second_number = 0, 1
+    for i in range(0, n):
+        first_number, second_number = second_number, first_number + second_number
 
-def fibo_rec(n):
-    if n==0 or n==1:
-        return n
-    else:
-        return fibo_rec(n-1)+fibo_rec(n-2)
+    return first_number
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    n=int(input("Zadej n pro výpočet Fibonacciho: "))
-    if n<0:
-        raise CustomException("N musí být >=0!")
-    else:
-        start_time = timeit.timeit()
-        fibo_n=[]
-        for my_counter in range(n+1):
-            if my_counter<2:
-                fibo_n.append(my_counter)
-            else:
-                temp=fibo_n[my_counter-1]+fibo_n[my_counter-2]
-                fibo_n.append(temp)
+    #n=int(input("Zadej n pro výpočet Fibonacciho: "))
+    for n in range (0,50):
+        if n<0:
+            raise CustomException("N musí být >=0!")
+        else:
+            start_time = timeit.timeit()
+            fibo_n=[]
+            for my_counter in range(n+1):
+                if my_counter<2:
+                    fibo_n.append(my_counter)
+                else:
+                    temp=fibo_n[my_counter-1]+fibo_n[my_counter-2]
+                    fibo_n.append(temp)
 
-        end_time = timeit.timeit()
+            end_time = timeit.timeit()
 
-        print("Doba výpočtu Fibonacciho iterací:",(end_time-start_time))
+            fibo_n1=fib_iter(n)
+            print(39*"-")
+            print(n)
+            print("Fibonacci iterací: ",fibo_n1)
 
-        print("Fibonacci iterací: ",fibo_n[n])
-
-        start_time = timeit.timeit()
-        fibo_n2=int(fibo_rec(n))
-        end_time = timeit.timeit()
-        print("Doba výpočtu Fibonacciho rekurzí:",(end_time - start_time))
-        print("Fibonacci rekurzí: ",fibo_n2)
+            fibo_n2=fib_rec(n)
+            end_time = timeit.timeit()
+            print("Fibonacci rekurzí: ",fibo_n2)
